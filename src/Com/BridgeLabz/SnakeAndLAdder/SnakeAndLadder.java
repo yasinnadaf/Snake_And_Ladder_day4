79b91843d75356386c30a2e16c383b1df17b2e2e
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-09 16:59:09 +0530

    Added: InitialisedPosition

--- src/Com/BridgeLabz/SnakeAndLAdder/SnakeAndLadder.java
+++ src/Com/BridgeLabz/SnakeAndLAdder/SnakeAndLadder.java
@@ -1,4 +1,10 @@
 package Com.BridgeLabz.SnakeAndLAdder;
 
 public class SnakeAndLadder {
+    static final int START_POSITION=0;
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Snake and ladder Game");
+    }
 }
