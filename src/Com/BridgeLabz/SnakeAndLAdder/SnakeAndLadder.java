a25a7d1a16af82946d1340991bda1c6fc6fafb33
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-09 17:14:10 +0530

    Updated: Dice Roll

--- src/Com/BridgeLabz/SnakeAndLAdder/SnakeAndLadder.java
+++ src/Com/BridgeLabz/SnakeAndLAdder/SnakeAndLadder.java
@@ -1,4 +1,15 @@
 package Com.BridgeLabz.SnakeAndLAdder;
 
 public class SnakeAndLadder {
+    static final int START_POSITION=0;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Game");
+
+        int dice= (int) (Math.random()*10)%6+1;
+        System.out.println("Dice rolled value: "+dice);
+
+    }
+
+
 }
