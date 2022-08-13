b6cb0603363bd1f693f9b2a8717b561223826c20
yasinnadaf <106942238+yasinnadaf@users.noreply.github.com>
2022-08-13 21:34:18 +0530

    Update SnakeAndLadder.java

--- src/Com/BridgeLabz/SnakeAndLAdder/SnakeAndLadder.java
+++ src/Com/BridgeLabz/SnakeAndLAdder/SnakeAndLadder.java
@@ -1,23 +1,23 @@
-package Com.BridgeLabz.SnakeAndLAdder;
+package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder {
-    static final int START_POSITION =0;
-    static final int NO_PLAY =0;
-    static final int IF_LADDER =1;
-    static final int IF_SNAKE =2;
-    static int currentposition=0;
+    static final int START_POSITION = 0;
+    static final int NO_PLAY = 0;
+    static final int IF_LADDER = 1;
+    static final int IF_SNAKE = 2;
+    static int currentposition = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and ladder game");
-        int dice= (int) (Math.random()*10)%6+1;
-        int option= (int)(Math.random()*10)%3;
+        int dice = (int) (Math.random()*10)%6+1;
+        int option = (int)(Math.random()*10)%3;
 
-        if(option==IF_LADDER){
+        if(option == IF_LADDER){
             System.out.println("Ladder!!");
             currentposition+=dice;
 
         }
-        else if(option==IF_SNAKE){
+        else if(option == IF_SNAKE){
             System.out.println("snake!!");
             currentposition-=dice;
 
