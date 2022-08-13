863bf04a196ee79e4509156e74cff96856751f99
yasinnadaf <106942238+yasinnadaf@users.noreply.github.com>
2022-08-13 22:33:57 +0530

    Update SnakeAndLadder.java

--- src/Com/BridgeLabz/SnakeAndLAdder/SnakeAndLadder.java
+++ src/Com/BridgeLabz/SnakeAndLAdder/SnakeAndLadder.java
@@ -1,13 +1,14 @@
-package Com.BridgeLabz.SnakeAndLAdder;
+package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder {
-    static final int START_POSITION=0;
+    
+    static final int START_POSITION = 0;
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Snake and Ladder Game");
 
-        int dice= (int) (Math.random()*10)%6+1;
+        int dice = (int) (Math.random()*10)%6+1;
         System.out.println("Dice rolled value: "+dice);
 
     }
